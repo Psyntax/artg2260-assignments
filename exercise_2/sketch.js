d4fb3b51e9246b3feb6adc945826bb2f66ad0d95
Psyntax <krawfish123@gmail.com>
2018-02-01 18:32:04 -0500

    yo
    fu

--- exercise_2/sketch.js
+++ exercise_2/sketch.js
@@ -8,21 +8,10 @@ function setup() {
 var x = 0
 var y = 0
 
-	for (x=0; x < 1000; x++) {
-	 
-		fill(205, 210, 214);
-		line(0,0,(0+(x*50)),500);
-
+	for (x=0; x < 100; x++) {
+		for (y=0; y<10; y++) {
+			line(0,y*50,(0+(x*50)),500);
+		}
 	}
-	
-	for (y=0; y < 1000; y++) {
-
-		stroke(255);
-		strokeWeight(1);
-
-		line(500,500,(500-(y*50)),0);
-	}
-
-
 	
 }
